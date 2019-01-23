ca2d2547f7a16511a63565fbce06d98e94b9f325
gretomaszek <44497722+gretomaszek@users.noreply.github.com>
2019-01-23 10:45:25 +0100

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import kalkulator from './kalkulator.vue'
+import VueSlideBar from 'vue-slide-bar'
+
+Vue.component('vue-slide-bar', VueSlideBar)
 
 new Vue({
     el: '#mbank-app',
