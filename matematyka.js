5a0f36a3035178b28b5ea0097d90fc8db2ba96c9
gretomaszek <44497722+gretomaszek@users.noreply.github.com>
2019-02-04 18:51:40 +0100

    Update matematyka.js

--- matematyka.js
+++ matematyka.js
@@ -1,11 +1,11 @@
-var okres = 100;
-var kwotaNetto = 109900;
-var rata = 1569;
-var oprocentowanie = 0.0799;
-var oprocentowanie2 = 0.09382252;
-var oprocentowanie3 = 0.09441188;
-var oprocentowanie4 = 0.09441287;
-var oprocentowanie5 = 0.09441287;
+var period = 100;
+var netValue = 109900;
+var instalmentValue = 1569;
+var interest = 0.0799;
+var interest2 = 0.09382252;
+var interest3 = 0.09441188;
+var interest4 = 0.09441287;
+var interest5 = 0.09441287;
 
 function tki(i) {
   return (i * 30) / 360;
@@ -13,9 +13,9 @@ function tki(i) {
 
 function aki(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (i - 1 <= okres) {
-    return rata * -1;
+    return netValue;
+  } else if (i - 1 <= period) {
+    return instalmentValue * -1;
   } else {
     return 0;
   }
@@ -23,11 +23,11 @@ function aki(i) {
 
 function fun1(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (aki(i) / Math.pow(1 + oprocentowanie, tki(i)) === 0) {
+    return netValue;
+  } else if (aki(i) / Math.pow(1 + interest, tki(i)) === 0) {
     return null;
   } else {
-    return aki(i) / Math.pow(1 + oprocentowanie, tki(i));
+    return aki(i) / Math.pow(1 + interest, tki(i));
   }
 }
 
@@ -35,22 +35,22 @@ function deriv1(i) {
   if (i === 0) {
     return null;
   } else if (
-    (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie, tki(i) + 1) ===
+    (aki(i) * tki(i)) / Math.pow(1 + interest, tki(i) + 1) ===
     0
   ) {
     return null;
   } else {
-    return (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie, tki(i) + 1);
+    return (aki(i) * tki(i)) / Math.pow(1 + interest, tki(i) + 1);
   }
 }
 
 function fun2(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (aki(i) / Math.pow(1 + oprocentowanie2, tki(i)) === 0) {
+    return netValue;
+  } else if (aki(i) / Math.pow(1 + interest2, tki(i)) === 0) {
     return null;
   } else {
-    return aki(i) / Math.pow(1 + oprocentowanie2, tki(i));
+    return aki(i) / Math.pow(1 + interest2, tki(i));
   }
 }
 
@@ -58,22 +58,22 @@ function deriv2(i) {
   if (i === 0) {
     return null;
   } else if (
-    (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie2, tki(i) + 1) ===
+    (aki(i) * tki(i)) / Math.pow(1 + interest2, tki(i) + 1) ===
     0
   ) {
     return null;
   } else {
-    return (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie2, tki(i) + 1);
+    return (aki(i) * tki(i)) / Math.pow(1 + interest2, tki(i) + 1);
   }
 }
 
 function fun3(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (aki(i) / Math.pow(1 + oprocentowanie3, tki(i)) === 0) {
+    return netValue;
+  } else if (aki(i) / Math.pow(1 + interest3, tki(i)) === 0) {
     return null;
   } else {
-    return aki(i) / Math.pow(1 + oprocentowanie3, tki(i));
+    return aki(i) / Math.pow(1 + interest3, tki(i));
   }
 }
 
@@ -81,22 +81,22 @@ function deriv3(i) {
   if (i === 0) {
     return null;
   } else if (
-    (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie3, tki(i) + 1) ===
+    (aki(i) * tki(i)) / Math.pow(1 + interest3, tki(i) + 1) ===
     0
   ) {
     return null;
   } else {
-    return (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie3, tki(i) + 1);
+    return (aki(i) * tki(i)) / Math.pow(1 + interest3, tki(i) + 1);
   }
 }
 
 function fun4(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (aki(i) / Math.pow(1 + oprocentowanie4, tki(i)) === 0) {
+    return netValue;
+  } else if (aki(i) / Math.pow(1 + interest4, tki(i)) === 0) {
     return null;
   } else {
-    return aki(i) / Math.pow(1 + oprocentowanie4, tki(i));
+    return aki(i) / Math.pow(1 + interest4, tki(i));
   }
 }
 
@@ -104,22 +104,22 @@ function deriv4(i) {
   if (i === 0) {
     return null;
   } else if (
-    (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie4, tki(i) + 1) ===
+    (aki(i) * tki(i)) / Math.pow(1 + interest4, tki(i) + 1) ===
     0
   ) {
     return null;
   } else {
-    return (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie4, tki(i) + 1);
+    return (aki(i) * tki(i)) / Math.pow(1 + interest4, tki(i) + 1);
   }
 }
 
 function fun5(i) {
   if (i === 0) {
-    return kwotaNetto;
-  } else if (aki(i) / Math.pow(1 + oprocentowanie5, tki(i)) === 0) {
+    return netValue;
+  } else if (aki(i) / Math.pow(1 + interest5, tki(i)) === 0) {
     return null;
   } else {
-    return aki(i) / Math.pow(1 + oprocentowanie5, tki(i));
+    return aki(i) / Math.pow(1 + interest5, tki(i));
   }
 }
 
@@ -127,17 +127,17 @@ function deriv5(i) {
   if (i === 0) {
     return null;
   } else if (
-    (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie5, tki(i) + 1) ===
+    (aki(i) * tki(i)) / Math.pow(1 + interest5, tki(i) + 1) ===
     0
   ) {
     return null;
   } else {
-    return (aki(i) * tki(i)) / Math.pow(1 + oprocentowanie5, tki(i) + 1);
+    return (aki(i) * tki(i)) / Math.pow(1 + interest5, tki(i) + 1);
   }
 }
 
 function resultantRate1(i) {
-  return oprocentowanie - fun1Sum / deriv1Sum;
+  return interest - fun1Sum / deriv1Sum;
 }
 
 function resultantRate2(i) {
@@ -164,10 +164,9 @@ function myRRSO(i) {
 //suma_pochodna1 = suma(pochodna1 2 + … pochodna1 i )
 //stopa_wynikowa2 = stopa_wynikowa1 - suma_funkcja2/ suma_pochodna2
 
-// tworzymy array z odpowiednią ilością pól
 var tabela = [];
 
-for (var i = 0; i <= okres; i++) {
+for (var i = 0; i <= period; i++) {
   tabela[i] = {
     tki: tki(i),
     aki: aki(i),
@@ -190,44 +189,44 @@ for (var i = 0; i <= okres; i++) {
   };
 }
 
-var fun1Sum = tabela.reduce(function(total, row) {
+var fun1Sum = tabela.reduce(function (total, row) {
   return total + row.fun1;
 }, 0);
 
-var deriv1Sum = tabela.reduce(function(total, row) {
+var deriv1Sum = tabela.reduce(function (total, row) {
   return total + row.deriv1;
 }, 0);
 
-var fun2Sum = tabela.reduce(function(total, row) {
+var fun2Sum = tabela.reduce(function (total, row) {
   return total + row.fun2;
 }, 0);
 
-var deriv2Sum = tabela.reduce(function(total, row) {
+var deriv2Sum = tabela.reduce(function (total, row) {
   return total + row.deriv2;
 }, 0);
 
-var fun3Sum = tabela.reduce(function(total, row) {
+var fun3Sum = tabela.reduce(function (total, row) {
   return total + row.fun3;
 }, 0);
 
-var deriv3Sum = tabela.reduce(function(total, row) {
+var deriv3Sum = tabela.reduce(function (total, row) {
   return total + row.deriv3;
 }, 0);
 
-var fun4Sum = tabela.reduce(function(total, row) {
+var fun4Sum = tabela.reduce(function (total, row) {
   return total + row.fun4;
 }, 0);
 
-var deriv4Sum = tabela.reduce(function(total, row) {
+var deriv4Sum = tabela.reduce(function (total, row) {
   return total + row.deriv4;
 }, 0);
 
-var fun5Sum = tabela.reduce(function(total, row) {
+var fun5Sum = tabela.reduce(function (total, row) {
   return total + row.fun5;
 }, 0);
 
-var deriv5Sum = tabela.reduce(function(total, row) {
+var deriv5Sum = tabela.reduce(function (total, row) {
   return total + row.deriv5;
 }, 0);
 
-console.log(resultantRate1(i));
+console.log(tabela);
