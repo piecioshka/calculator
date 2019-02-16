47972bc75abad46ddbe703551602d5604001fe9d
Piotr Kowalski <piecioshka@gmail.com>
2019-02-16 12:06:42 +0100

    Profiling

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,5 @@ $('.timeSlider').on('input', _.debounce(update, 50));
 update();
 
 function update() {
-    console.time();
     init(period, netValue, percent, monthlyRate);
-    console.timeEnd();
 }
